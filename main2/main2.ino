38299496dcf23de2eafc3774536774e5e41e3796
Mees Meijer <Mees.r.meijer@outlook.com>
2022-11-16 20:17:48 +0100

    [+] laatste files

--- main2/main2.ino
+++ main2/main2.ino
@@ -1,31 +1,55 @@
+
 #include "esp_adc_cal.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <string>
-// #include <WiFi.h>
+#include <WebSocketsClient.h>
 
-#define PIN_ANALOG_IN 4                
+#include <string>
+#include <WiFi.h>
+#include <WiFiManager.h>
+#include <Preferences.h>
+
+#define PIN_ANALOG_IN 34              
 #define DEFAULT_VREF 1100              
 #define NO_OF_SAMPLES 64               
 
 #define BUZZER_PIN 2
-#define THERMOLIED_PIN 4 
+#define THERMOLIED_PIN 34 
 #define SDA_LCD 19
 #define SCL_LCD 18
 
-adc_channel_t channel = ADC_CHANNEL_0; 
-adc_unit_t unit = ADC_UNIT_2;
-adc_atten_t atten = ADC_ATTEN_DB_11;   
-esp_adc_cal_characteristics_t *adc_chars;
-esp_adc_cal_value_t val_type;
 hw_timer_t *Timer = NULL;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+Preferences localStorage;
+WebSocketsClient webSocket;
 
-struct Koelkast{
-    float highestTemp = 10.0;
-    float lowestTemp = 0;
 
-    bool hasUpdate = false; 
+enum State{
+    STARTING = 0,
+    ERROR = 1,
+    
+    CONNECTED = 2,
+    RE_CONNECTING = 3,
+    DISCONNECTED = 4,
+
+    TO_HIGH = 5,
+    TO_LOW = 6,
+
+    GOT_IP = 7,
+    OPEN_DOOR = 8,
+
+    NORMAL = 9,
+};
+
+struct Object{
+    float highestTemp = 24.0;
+    float lowestTemp = 22.0;
+
+    bool hasUpdate = false;
+
+    int state = State::NORMAL;
+    int wsState = State::NORMAL;
+    int doorState = State::NORMAL; 
 };
 
 struct Door{
@@ -41,20 +65,99 @@ struct TimerT{
     int hours = 12;
 };
 
-
 TimerT clock1; 
 Door door; 
-Koelkast koelkast; 
+Object koelkast; 
 
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16)
+{
+    const uint8_t *src = (const uint8_t *)mem;
+    Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+    for (uint32_t i = 0; i < len; i++)
+    {
+        if (i % cols == 0)
+        {
+            Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+        }
+        Serial.printf("%02X ", *src);
+        src++;
+    }
+    Serial.printf("\n");
+}
+
+
+void handleSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+    String strLoad = "";
+
+    switch (type){
+        case WStype_DISCONNECTED:
+            Serial.printf("[WSc] Disconnected!\n");
+            koelkast.wsState = State::DISCONNECTED;
+            break;
+        case WStype_CONNECTED:
+            Serial.printf("[WSc] Connected to url: %s\n", payload);
+            webSocket.sendTXT("Connected");
+            koelkast.wsState = State::CONNECTED;
+            break;
+
+        case WStype_TEXT:
+            Serial.printf("[WSc] get text: %s\n", payload);
+
+            strLoad = (char* )payload;
+            // SET_TEMP: 14.0 | 19.0
+            if (strLoad.startsWith("AUTH")){
+                Serial.println("Authentication send");
+                webSocket.sendTXT("AUTH:SECRET_TOKEN");
+
+            } else if (strLoad.startsWith("SET_TEMP_MIN")){
+                strLoad.replace("SET_TEMP_MIN:", "");
+                koelkast.lowestTemp = strLoad.toFloat();
+                Serial.println("Min now "+ (String) koelkast.lowestTemp + " been"+ strLoad);
+
+            }else if (strLoad.startsWith("SET_TEMP_MAX")){
+                strLoad.replace("SET_TEMP_MAX:", "");
+                koelkast.highestTemp = strLoad.toFloat();
+                Serial.println("Max now "+ (String) koelkast.highestTemp + " been"+ strLoad);
+
+            }else if (strLoad.startsWith("SET_HOUR")){
+                strLoad.replace("SET_HOUR:", "");
+                clock1.hours = strLoad.toInt();
+                Serial.println("Hours now "+ (String)clock1.hours + " been"+ strLoad);
+
+            }else if (strLoad.startsWith("SET_MINS")){
+                strLoad.replace("SET_MINS:", "");
+                clock1.mins = strLoad.toInt();
+                Serial.println("Mins now "+ (String)clock1.mins + " been"+ strLoad);
+
+            }else if (strLoad.startsWith("SET_SEC")){
+                strLoad.replace("SET_SEC:", "");
+                clock1.secs = strLoad.toInt();
+                Serial.println("Secs now "+ (String)clock1.secs + " been"+ strLoad);
+            }
+            break;
+        case WStype_BIN:
+            Serial.printf("[WSc] get binary length: %u\n", length);
+            hexdump(payload, length);
+            break;
+
+        case WStype_ERROR:
+        case WStype_FRAGMENT_TEXT_START:
+        case WStype_FRAGMENT_BIN_START:
+        case WStype_FRAGMENT:
+        case WStype_FRAGMENT_FIN:
+            break;
+    }
+}
 
 void IRAM_ATTR doorInterrupt(){
     long unsigned currentTime = millis();
 
-    if ((currentTime - door.lastOpen) > 300){
+    // if ((currentTime - door.lastOpen) > 1000){
         door.open = true;
         door.changed = true; 
         door.lastOpen = currentTime;    
-    }
+    // }
 }
 
 void IRAM_ATTR timerInterrupt(){
@@ -77,31 +180,70 @@ void IRAM_ATTR timerInterrupt(){
     koelkast.hasUpdate = true; 
 }
 
-void updateDisplay(float tempInC){
+void updateDisplay(float tempInC = 0.0){
    
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.printf("%.1f C | | %.i:%.i", tempInC, clock1.hours, clock1.mins);
+
+    String mins = (String)clock1.mins;
+    String Hours = (String)clock1.hours;
+
+    if (clock1.mins < 10.0){
+        mins = "0" + mins;
+    }
+    if (clock1.hours < 10.0){
+        Hours = "0"+Hours;
+    }
+
+    lcd.printf("%.1f C   %s:%s", tempInC, Hours, mins);
     lcd.setCursor(0,1);
-    lcd.printf("%s", "testing");
+
+    switch (koelkast.wsState){
+        case State::STARTING:
+            lcd.printf("%s", "Starting..");
+            break;
+
+        case State::CONNECTED:
+            lcd.printf("%s", "Ws Connected");
+            koelkast.wsState = State::NORMAL;
+            break;
+
+        case State::RE_CONNECTING:
+            lcd.printf("%s", "Re-connecting");
+            break;    
+
+        case State::DISCONNECTED:
+            lcd.printf("%s", "Disconnected");
+            break;
+
+        case State::TO_HIGH:
+            lcd.printf("%s", "TO HIGH");
+            break;
+
+        case State::TO_LOW:
+            lcd.printf("%s", "TO LOW");
+            break;
+
+        case State::ERROR:
+            lcd.printf("%s", "Error..");
+            break;
+
+        case State::GOT_IP:
+            lcd.printf("IP: %s", WiFi.localIP());
+            break;
+        
+        case State::OPEN_DOOR:
+            lcd.printf("%s", "Door Open");
+            break;
+        
+        case State::NORMAL:
+            lcd.printf("%s", "Normal");
+            break;
+    };
 }
 
 float getRoomTemp(){
-    uint32_t adc_reading = 0;
-
-    for (int i = 0; i < NO_OF_SAMPLES; i++){
-        if (unit == ADC_UNIT_1){
-            adc_reading += adc1_get_raw((adc1_channel_t)channel);
-
-        } else{
-            int raw;
-            adc2_get_raw((adc2_channel_t)channel, ADC_WIDTH_BIT_12, &raw);
-            adc_reading += raw;
-        }
-    }
-
-    adc_reading /= NO_OF_SAMPLES;
-    uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+    uint32_t voltage = analogReadMilliVolts(34);
     double vol = voltage / 1000.0f;
     double Rt = 10 * vol / (3.3 - vol);                             
     double tempK = 1 / (1 / (273.15 + 25) + log(Rt / 10) / 3950.0); // Temp in kelvin
@@ -109,14 +251,15 @@ float getRoomTemp(){
     return tempC; 
 }
 
-
 void setup(){
+    WiFi.mode(WIFI_STA);
+
     // Basic setup esp32 
     Serial.begin(115200);
 
-    pinMode(door.pin, INPUT_PULLUP);
+    // pinMode(door.pin, INPUT_PULLDOWN);
     pinMode(BUZZER_PIN, OUTPUT);
-    attachInterrupt(door.pin, doorInterrupt, FALLING);
+    // attachInterrupt(door.pin, doorInterrupt, RISING);
 
     // setup lcd op pin 18, 19
     Wire.begin(SDA_LCD, SCL_LCD);
@@ -130,39 +273,66 @@ void setup(){
     timerAlarmWrite(Timer, 1000000, true);
     timerAlarmEnable(Timer);
 
+    WiFiManager wm;
+    bool res;
 
-    // ADC callibratie 
-    if (unit == ADC_UNIT_1){  
-        adc1_config_width(ADC_WIDTH_BIT_12);
-        adc1_config_channel_atten((adc1_channel_t)channel, atten);
+    res = wm.autoConnect("AutoConnectAP","password");
+    if (!res){
+        Serial.println("Coudnt connected..");
+    }else{
+        // Serial.println(WiFi.isConnected());
+        Serial.println("Conencted to Wifi");
     }
-    else{
-        adc2_config_channel_atten((adc2_channel_t)channel, atten);
-    }
-    adc_chars = (esp_adc_cal_characteristics_t *)calloc(1,sizeof(esp_adc_cal_characteristics_t));
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+
+    webSocket.enableHeartbeat(1000, 6000, 0);
+    webSocket.beginSSL("project1.meesinc.nl", 443);
+    webSocket.onEvent(handleSocketEvent);
+    webSocket.setExtraHeaders("AUTHENTICATION: SECRET_TOKEN_2022");
+    webSocket.setExtraHeaders("UUID: 1");
+    webSocket.setReconnectInterval(1000);
 }
 
 void loop(){
+    webSocket.loop();
+    
     // true every second. 
     if (koelkast.hasUpdate){
         float readTemp = getRoomTemp();
-        updateDisplay(readTemp);
+        // int state = State::NORMAL;
+        koelkast.wsState = State::NORMAL;
         
-        Serial.printf("Temp: %.2f C\n", readTemp);
-        
-        if (readTemp > koelkast.highestTemp){
-            Serial.print("temp to HIGH\n");
+        if (door.changed){
+            if (door.open){
+                koelkast.wsState = State::OPEN_DOOR;
+                door.changed = false; 
+            } else {
+                koelkast.wsState = State::NORMAL;
+            }
         }
-    
+
+        if (readTemp > koelkast.highestTemp){
+            koelkast.wsState = State::TO_HIGH;
+
+            if (door.open && ( (millis() - door.lastOpen) < (2 * 60 * 1000))){
+                // Serial.println("Door open for to long..");
+            }
+
+        }else if (readTemp < koelkast.lowestTemp){
+            koelkast.wsState = State::TO_LOW;
+
+            if (( (millis() - door.lastOpen) > (2 * 60 * 1000))){
+                // Serial.println("Koelkast is kapot.. check de deur!");
+            }
+        }
+
+        if (webSocket.isConnected()){
+            webSocket.sendTXT("READINGS:"+(String)readTemp+ "|"+ (String)door.open);
+        }
+        
+        updateDisplay(readTemp);
         koelkast.hasUpdate = false;
     }
-    if (door.changed){
-        if (door.open){
-            Serial.println("Door opennd..");
-            door.changed = false; 
-        }
-    }
+   
     // delay(1000);
     
 }
